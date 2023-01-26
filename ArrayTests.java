661e282d8b50cbd4735395ac41e04554acfcbcd0
Brian <122561802+BrianMorse12604@users.noreply.github.com>
2023-01-26 10:50:25 -0800

    Fixing the reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,11 +2,16 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
+	/*
+  @Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3,2,1}, input2);
 	}
 
 
@@ -14,5 +19,24 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input2));
   }
+  */
+  @Test
+  public void testReverseInPlaceMirror() {
+    int[] input = {1,2,1};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] {1,2,1}, input);
+  }
+
+  @Test
+  public void testReverseInPlaceRegular() {
+    int[] input = {1,2,3};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] {3,2,1}, input);
+  }
+
+
 }
